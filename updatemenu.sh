0d8babea77391dcec1d0ac63f9d9158bc0ef63e3
adir95 <adiriyanto95@gmail.com>
2017-04-11 12:50:08 +0700

    Update updatemenu.sh

--- updatemenu.sh
+++ updatemenu.sh
@@ -2,6 +2,6 @@
 
 # Install Menu
 cd
-wget "https://raw.githubusercontent.com/adir95/deb7/master/menu/menu"
+wget "https://raw.githubusercontent.com/adir95/deb7/master/menu/menu0"
 mv ./menu /usr/local/bin/menu
 chmod +x /usr/local/bin/menu
