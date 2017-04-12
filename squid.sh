4ebe51df2a218632b61475e3de9a80aa8b7bd38d
adir95 <adiriyanto95@gmail.com>
2017-04-12 12:05:33 +0700

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #install squid3
 apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/adir95/deb7/master/conf/squid3.conf"
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/adir95/deb7/master/conf/squid.conf"
 sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
