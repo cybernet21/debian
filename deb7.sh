776aaf0929f580630767e09c78bc0b877380e039
adir95 <adiriyanto95@gmail.com>
2017-04-24 21:39:00 +0700

    Update deb7.sh

--- deb7.sh
+++ deb7.sh
@@ -236,6 +236,22 @@ cd
 #clearcache
 #echo 1 > /proc/sys/vm/drop_caches
 
+# swap ram
+dd if=/dev/zero of=/swapfile bs=1024 count=1024k
+# buat swap
+mkswap /swapfile
+# jalan swapfile
+swapon /swapfile
+#auto star saat reboot
+wget https://raw.githubusercontent.com/adir95/deb7/master/ram/fstab
+mv ./fstab /etc/fstab
+chmod 644 /etc/fstab
+sysctl vm.swappiness=10
+#permission swapfile
+chown root:root /swapfile 
+chmod 0600 /swapfile
+
+
 # finalisasi
 chown -R www-data:www-data /home/vps/public_html
 service nginx start
